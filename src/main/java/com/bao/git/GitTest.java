d37f9fad9dea0f2c9ca117b30ec238ed37336b7b
xiong <3372998125@qq.com>
2022-08-04 16:10:16 +0800

    push commit2

--- src/main/java/com/bao/git/GitTest.java
+++ src/main/java/com/bao/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("萝卜4");
         System.out.println("萝卜master");
         System.out.println("萝卜5");
+        System.out.println("pushtest");
     }
 }
